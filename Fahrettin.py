4366ba2be680101fd7c06369977d8a0edcef4872
reelman543 <reelman543@gmail.com>
2025-10-08 01:15:45 +0300

    Update Fahrettin.py

--- Fahrettin.py
+++ Fahrettin.py
@@ -70,7 +70,7 @@ def find_baseurl(url):
 def generate_m3u(base_url, referer, user_agent):
     lines = ["#EXTM3U"]
     for idx, k in enumerate(KANALLAR, start=1):
-        name = f"GUNLU {k['kanal_adi']}"
+        name = f"TR {k['kanal_adi']}"
         lines.append(f'#EXTINF:-1 tvg-id="{k["tvg_id"]}" tvg-name="{name}",{name}')
         lines.append(f'#EXTVLCOPT:http-user-agent={user_agent}')
         lines.append(f'#EXTVLCOPT:http-referrer={referer}')
